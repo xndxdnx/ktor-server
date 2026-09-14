29dad3c27a7c29bb202fe0db2a4e51542a7929d8
Vlad <xndxdnx@gmail.com>
2026-09-14 16:49:20 +0300

    Created project

---
+++ build.gradle.kts
@@ -0,0 +1,24 @@
+
+plugins {
+    alias(libs.plugins.kotlin.jvm)
+    alias(ktorLibs.plugins.ktor)
+}
+
+group = "com.example"
+version = "1.0.0-SNAPSHOT"
+
+application {
+    mainClass = "io.ktor.server.netty.EngineMain"
+}
+
+kotlin {
+    jvmToolchain(21)
+}
+dependencies {
+    implementation(ktorLibs.server.core)
+    implementation(ktorLibs.server.netty)
+    implementation(libs.logback.classic)
+
+    testImplementation(kotlin("test"))
+    testImplementation(ktorLibs.server.testHost)
+}
---
+++ src/main/kotlin/main.kt
@@ -0,0 +1,5 @@
+package com.example
+
+fun main(args: Array<String>) {
+    io.ktor.server.netty.EngineMain.main(args)
+}
